98dca500dc2f4fcf914cbc499befb73c86099a7d
YukineQ <mayberainq@gmail.com>
2023-12-22 16:07:28 +0300

    img opacity loading

--- src/components/ui/image/image.tsx
+++ src/components/ui/image/image.tsx
@@ -2,6 +2,7 @@ import NextImage, { ImageProps as NextImageProps } from 'next/image'
 import React from 'react'
 import { Skeleton } from '..'
 import { BASE_URL } from '@/config/api';
+import { twMerge } from 'tailwind-merge';
 
 type ImageProps = NextImageProps & {
     fromBaseApi?: boolean;
@@ -21,10 +22,11 @@ export const Image = ({ fromBaseApi = true, src, ...props }: ImageProps) => {
                     onLoad={() => setIsLoaded(true)}
                     onError={() => setIsBroken(true)}
                     sizes='100vw'
+                    className={twMerge(!isLoaded ? 'opacity-0' : 'opacity-100', 'transition')}
                     {...props}
                 />
                 {!isLoaded &&
-                    (<Skeleton className='w-full h-full rounded-none' />)}
+                    (<Skeleton className='w-full h-full rounded-none animate-none' />)}
             </>
         )
     }
